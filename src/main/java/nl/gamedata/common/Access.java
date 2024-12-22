ede1c7ec83d36c4076e9e8d8ca7f66065069b3f9
Alexander Verbraeck <a.verbraeck@gmail.com>
2024-12-22 14:45:11 +0100

    Access levels for data in the database (CRUD)

---
+++ src/main/java/nl/gamedata/common/Access.java
@@ -0,0 +1,44 @@
+package nl.gamedata.common;
+
+/**
+ * Access to a table or part of the database.
+ * <p>
+ * Copyright (c) 2024-2024 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="https://github.com/averbraeck/gamedata-common/LICENSE">GameData project License</a>.
+ * </p>
+ * @author <a href="https://github.com/averbraeck">Alexander Verbraeck</a>
+ */
+public enum Access
+{
+    /** Create access. */
+    CREATE,
+
+    /** Edit access. */
+    EDIT,
+
+    /** View access. */
+    VIEW,
+
+    /** No access. */
+    NONE;
+
+    public boolean create()
+    {
+        return this.equals(CREATE);
+    }
+
+    public boolean edit()
+    {
+        return this.equals(CREATE) || this.equals(EDIT);
+    }
+
+    public boolean view()
+    {
+        return !this.equals(NONE);
+    }
+
+    public boolean none()
+    {
+        return this.equals(NONE);
+    }
+}
